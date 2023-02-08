7308b2ffc8b3012a635cd8790fcea4ee65d48d5d
unendlicherPing <werdaslesenkannistschlau@gmail.com>
2023-02-08 17:20:40 +0100

    feature(options): Removed Statusline Removed statusline completely

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -11,6 +11,12 @@ function M.setup()
 	-- show hybride line numbers
 	set.number = true
 	set.relativenumber = true
+
+	-- remove statusline
+	set.showmode = false
+        set.ruler = false
+        set.laststatus = 0
+        set.showcmd = false
 end
 
 return M
