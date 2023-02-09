d4d0c014976e120d89d97eeb14abaf30e7e4be9d
unendlicherPing <werdaslesenkannistschlau@gmail.com>
2023-02-09 16:25:52 +0100

    feature: Lazy loaded mappings

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
 require("user.lazy").setup()
 require("user.options").setup()
-require("user.mappings").setup()
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+        require("user.mappings").setup()
+    end,
+})
