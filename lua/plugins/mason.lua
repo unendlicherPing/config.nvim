bd4d2cce18d74ce0709212701fcf144a96b76da6
unendlicherPing <werdaslesenkannistschlau@gmail.com>
2023-02-11 00:59:18 +0100

    feat(mason): Add lsp and diagnostic features
    Add basic commands for navigating through a codebase

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,7 +8,23 @@ M.dependencies = {
     "neovim/nvim-lspconfig",
 }
 
-local function on_attach() end
+local function on_attach(_, buffer_number)
+    vim.api.nvim_buf_set_option(buffer_number, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Diagnostic hint", buffer = buffer_number })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Goto declaration", buffer = buffer_number })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Goto definition", buffer = buffer_number })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "hove", buffer = buffer_number })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Goto implementation", buffer = buffer_number })
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = "Signature help", buffer = buffer_number })
+    vim.keymap.set('n', "<leader>cD", vim.lsp.buf.type_definition,
+        { desc = "Goto type definition", buffer = buffer_number })
+    vim.keymap.set('n', '<space>cf', vim.lsp.buf.rename, { desc = "Rename", buffer = buffer_number })
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, { desc = "Action", buffer = buffer_number })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Goto references", buffer = buffer_number })
+    vim.keymap.set('n', '<space>cf', function() vim.lsp.buf.format { async = true } end,
+        { desc = "Format", buffer = buffer_number })
+end
 
 local lsp_flags = {
     -- This is the default in Nvim 0.7+
@@ -21,7 +37,7 @@ function M.config()
 
     local runtime_path = vim.split(package.path, ";")
     table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
+    table.insert(runtime_path, "lua/?/?.lua")
 
     local nvim_lsp = require("lspconfig")
 
