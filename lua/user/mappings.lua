4c8c07fd683800a976401274dfa9b5486fc20db3
unendlicherPing <werdaslesenkannistschlau@gmail.com>
2023-02-11 22:17:32 +0100

    feat(mappings): add mapping for exit terminal mode

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup()
 	vim.keymap.set("i", "jk", "<esc>", { desc = "escape" })
+	vim.keymap.set("t", "jk", "<c-\\><c-n>", { desc = "escape terminal mode" })
 end
 
 return M
